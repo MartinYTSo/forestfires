bc4dc510d4a7bc7f2777cbe4819ec506313ea47c
martinytso <martinytso99@gmail.com>
2025-03-06 22:55:50 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,68 +2,78 @@ import streamlit as st
 import requests
 import numpy as np
 import folium 
+from streamlit_folium import st_folium
 
 st.set_page_config(layout="wide",
-                   initial_sidebar_state = "expanded",
-                       menu_items={'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    })
+                   initial_sidebar_state = "expanded")
 
 st.title("CSE 6242 Predicting Real Estate Risks via Forest Fires")
 
 
 st.sidebar.info(
     """
-    - Web App URL: <https://streamlit.gishub.org>
+    - Web App URL: http://xxx.com
+    \n
     - GitHub repository: <https://github.com/martinytso/forestfires>
     """
 )
 
-st.sidebar.title("Contact")
+st.sidebar.title("Made By")
 st.sidebar.info(
     """
-CSE 6242 Data and Visual Analytics  Team 12 |Ryan Cherry, Martin So | Lida Goldchteine| Katrina Silvorski
+  Team 12\n 
+  Ryan Cherry, Martin So, Lida Goldchteine, Katrina Silvorski
     """
 )
 
-with st.container(border=True) as container:
+row1= st.container(border=True)
+with row1:
     st.write("This is inside the container")
 
     # Create a two-column layout inside the container
-    col1, col2,col3,col4 = st.columns(4, border=True)
+    row1_col1, row1_col2,row1_col3,row1_col4 = st.columns(4, border=True)
 
-    with col1:
-        col1.write("This is Column 1 inside the container")
+    with row1_col1:
+        row1_col1.write("This is Column 1 inside the container")
 
-    with col2:
-        col2.write("This is Column 2 inside the container")
+    with row1_col2:
+        row1_col2.write("This is Column 2 inside the container")
 
-    with col3:
-        col3.write("This is Column 3 inside the container")
-    with col4:
-        col4.write("This is Column 4 inside the container")
+    with row1_col3:
+        row1_col3.write("This is Column 3 inside the container")
+    with row1_col4:
+        row1_col4.write("This is Column 4 inside the container")
 
-map= st.container(border=True)
+row2= st.container(border=True)
 
-with map:
-    map.write("Test - This should be in the middle.")
+with row2:
+    row2_col1, row2_col2,row2_col3 = st.columns(3, border=True)
+    with row2_col1:
+        row2_col1.write("This is Column 1 inside the container")
+
+    with row2_col2:
+        row2_col2.write("This is Column 2 inside the container")
+
+    with row2_col3:
+        row2_col3.write("This is Column 3 inside the container")
 
 
-row3_col1, row3_col2 = st.columns([6, 1],border=True)
+row3_row1_col1, row3_row1_col2 = st.columns([5, 1],border=True)
+
 
 # Fetch the folium map from the FastAPI server
+map_url = "https://forestfires-gy35.onrender.com"
+response = requests.get(map_url)
 
-with row3_col1:
-    map_url = "https://forestfires-gy35.onrender.com"
-    response = requests.get(map_url)
+##########################################
 
-    if response.status_code == 200:
-            
-            st.components.v1.html(response.text, width=1000000,height=800)
-    else:
-        st.error("Failed to load the map. Ensure the FastAPI server is running.")
-with row3_col2:
+
+with row3_row1_col1:
+    st.components.v1.html(response.text, width=1680,height=1080)
+    # st_data=st_folium(m, width=1000, height=440)
+
+with row3_row1_col2:
     st.write("Additional information or content can be placed here.")
+
 
 
